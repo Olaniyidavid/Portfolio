0e639efe6c8fe0f8e623060f257e53f6235135ae
Olaniyidavid <55438610+Olaniyidavid@users.noreply.github.com>
2025-01-21 09:33:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+} // Close the toggleMenu function
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  // Store the user's preference in localStorage
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
 }
+
+// Check the user's preference on page load
+window.onload = function () {
+  if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+  }
+};
